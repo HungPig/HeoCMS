9d2d12b35da2a0591fdd55edb24d4c39b1a0815a
Thanh Hung <hungthanhhung37@gmail.com>
2025-01-25 00:06:05 +0700

    Update Database In SQL

--- HeoBlog.Api/Program.cs
+++ HeoBlog.Api/Program.cs
@@ -1,12 +1,47 @@
 
+using HeoBlog.Core.Domain.Edentity;
+using HeoBlog.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace HeoBlog.Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
 
+            var builder = WebApplication.CreateBuilder(args);
+            // add configuration va` string vao 
+            var configuration = builder.Configuration;
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            // Add BdContext Config and ASP.CORE Identity
+            builder.Services.AddDbContext<HeoBlogContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<HeoBlogContext>();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
             // Add services to the container.
 
             builder.Services.AddControllers();
---
+++ HeoBlog.Data/HeoBlogContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeoBlog.Data
+{
+    public class HeoBlogContextFactory : IDesignTimeDbContextFactory<HeoBlogContext>
+    {
+        public HeoBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<HeoBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new HeoBlogContext(builder.Options);
+        }
+    }
+}
