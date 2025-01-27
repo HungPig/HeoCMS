0860fe97bf13945e71a7d90a2913aa5818cd0e81
Thanh Hung <hungthanhhung37@gmail.com>
2025-01-27 16:16:08 +0700

    Seeding Approle

---
+++ HeoBlog.Api/MigrationManager.cs
@@ -0,0 +1,21 @@
+﻿using HeoBlog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeoBlog.Api
+{
+    public static class MigrationManager
+    {
+        public static WebApplication MigrateDatabase(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetRequiredService<HeoBlogContext>())
+                {
+                    context.Database.Migrate();
+                    new DataSeeder().SeedAsync(context).Wait();
+                }
+            }
+            return app;
+        }
+    }
+}
